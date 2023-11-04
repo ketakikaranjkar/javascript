bbb98646ef300c3ae5a9b43b553cdb21cdd08fa8
ketakikaranjkar <ketaki1karanjkar@gmail.com>
2023-11-04 23:16:16 +0530

    js

---
+++ index.js
@@ -0,0 +1,23 @@
+function addition(a, b) {
+  console.log(a + b);
+}
+
+addition(2, 3);
+
+function subtraction(a, b) {
+  console.log(a - b);
+}
+
+subtraction(2, 3);
+
+function multiplication(a, b) {
+  console.log(a * b);
+}
+
+multiplication(2, 3);
+
+function division(a, b) {
+  console.log(a / b);
+}
+
+division(9, 9);
